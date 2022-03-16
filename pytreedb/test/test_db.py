8c5ffa7873c8c5929121ee1a6e190b5a9b773b47
han16nah <han_nah@mailbox.org>
2022-03-16 15:21:10 +0100

    add notes for testing

--- pytreedb/test/test_db.py
+++ pytreedb/test/test_db.py
@@ -2,3 +2,33 @@
 # -- coding: utf-8 --
 
 import pytest
+
+# functions to test:
+
+##### import
+# save()
+# export_data()
+# export_to_csv()
+
+#### export
+# initiate db
+# load()
+# clear()
+# import_data()
+# import_db()
+
+#### query
+# indexing db
+# different queries
+# also cases where we expect errors to be caught
+# get_ids
+
+# get_tree_as_json (is this function even needed?)
+# validate_json()
+
+#### utils
+
+# needed: small db for fast testing purposes?
+
+# also think beyond: e.g., users with their own data with different tree properties
+# make sure these can also be queried!

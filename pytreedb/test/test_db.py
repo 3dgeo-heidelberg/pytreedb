afa25dceee4cdb0e9a76108be1a7de2fa0297ec5
han16nah <han_nah@mailbox.org>
2022-03-18 08:30:30 +0100

    avoid code duplication by using the pytest.parameterize marker

--- pytreedb/test/test_db.py
+++ pytreedb/test/test_db.py
@@ -83,7 +83,11 @@ def test_export_data_content(tmp_path):
 
 
 @pytest.mark.export
-def test_convert_to_csv_general(tmp_path):
+@pytest.mark.parametrize('i, trees',
+                         [(2, None),
+                          (0, [0, 2])
+                          ])
+def test_convert_to_csv_general(tmp_path, i, trees):
     """Test function to export data to csv"""
     my_dbfile = tmp_path / "temp.db"
     input_data = f"{dir_path}/data/test/test_geojsons"
@@ -93,56 +97,25 @@ def test_convert_to_csv_general(tmp_path):
         dbfile=my_dbfile, mongodb={"uri": conn_uri, "db": conn_db, "col": conn_col}
     )
     mydb.import_data(input_data)
-    mydb.convert_to_csv(outdir_csv)
 
     all_jsons = list(Path(input_data).glob("*.*json"))
-    i = 2
     one_json = all_jsons[i]
     csv_general = tmp_path / "result_general.csv"
 
     with open(one_json) as f:
         data_dict = json.load(f)
+
+    if trees:
+        mydb.convert_to_csv(outdir_csv, trees=trees)
+        n = len(trees)
+    else:
+        mydb.convert_to_csv(outdir_csv)
+        n = len(all_jsons)
+
     df_general = pd.read_csv(csv_general)
 
     # table should contain as many rows as there are trees (= jsons files)
-    assert df_general.shape[0] == len(all_jsons)
-    # table should contain: tree_id, species, lat_epsg4326, long_epsg4326, elev_epsg4326
-    assert {"tree_id", "species", "lat_epsg4326", "long_epsg4326", "elev_epsg4326"}.issubset(df_general.columns)
-    # content of table should match with geojson
-    assert df_general.loc[i]["tree_id"] == data_dict["properties"]["id"]
-    assert df_general.loc[i]["species"] == data_dict["properties"]["species"]
-    np.testing.assert_equal(
-                            df_general.loc[i][["long_epsg4326", "lat_epsg4326", "elev_epsg4326"]].to_numpy(),
-                            data_dict["geometry"]["coordinates"]
-    )
-
-
-@pytest.mark.export
-def test_convert_subset_to_csv_general(tmp_path):
-    """Test function to export subset of data to csv"""
-    my_dbfile = tmp_path / "temp.db"
-    input_data = f"{dir_path}/data/test/test_geojsons"
-    outdir_csv = tmp_path
-
-    mydb = db.PyTreeDB(
-        dbfile=my_dbfile, mongodb={"uri": conn_uri, "db": conn_db, "col": conn_col}
-    )
-    mydb.import_data(input_data)
-
-    all_jsons = list(Path(input_data).glob("*.*json"))
-    i = 0
-    trees = [i, i+2]
-    one_json = all_jsons[i]
-    mydb.convert_to_csv(outdir_csv, trees=trees)
-
-    csv_general = tmp_path / "result_general.csv"
-
-    with open(one_json) as f:
-        data_dict = json.load(f)
-    df_general = pd.read_csv(csv_general)
-
-    # table should contain as many rows as there are trees (= jsons files)
-    assert df_general.shape[0] == len(trees)
+    assert df_general.shape[0] == n
     # table should contain: tree_id, species, lat_epsg4326, long_epsg4326, elev_epsg4326
     assert {"tree_id", "species", "lat_epsg4326", "long_epsg4326", "elev_epsg4326"}.issubset(df_general.columns)
     # content of table should match with geojson

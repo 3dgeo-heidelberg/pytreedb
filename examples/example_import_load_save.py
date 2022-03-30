58b8415c6257cf0b25e6ec782eb7290262c975a2
han16nah <han_nah@mailbox.org>
2022-03-30 12:01:50 +0200

    replace download links (use GitHub links) in tests and example scripts

--- examples/example_import_load_save.py
+++ examples/example_import_load_save.py
@@ -8,36 +8,37 @@ print("  Run some example imports...")
 print("##########################################")
 
 print("# Create pytreedb - incl. local file and mongoDB connection") 
-mydbfile='my_first_pytree.db' #file where database is stored locally additionally to MongoDB
+mydbfile = 'my_first_pytree.db'  # file where database is stored locally additionally to MongoDB
 
 print("# Create PyTreeDB object and read MongoDB connection from local .env file")
 mydb = db.PyTreeDB(dbfile=mydbfile)
 
-print("# Fresh import data from into pytreedb from URL which provides zipped folder with geojson files of each tree to be added")
-mydb.import_data(r'https://heibox.uni-heidelberg.de/f/05969694cbed4c41bcb8/?dl=1', overwrite=True)
-print(mydb.get_stats()) # print short stats after import
+print("# Fresh import data from into pytreedb from URL which provides zipped folder with geojson files of each tree "
+      "to be added")
+mydb.import_data(r'https://github.com/3dgeo-heidelberg/pytreedb/raw/main/data/test/geojsons.zip', overwrite=True)
+print(mydb.get_stats())  # print short stats after import
 
-print ("# Import from URL with GeoJSON files but appending not clearing the pytreedb first")
-mydb.import_data(r'https://heibox.uni-heidelberg.de/f/05969694cbed4c41bcb8/?dl=1', overwrite=False)
+print("# Import from URL with GeoJSON files but appending not clearing the pytreedb first")
+mydb.import_data(r'https://github.com/3dgeo-heidelberg/pytreedb/raw/main/data/test/geojsons.zip', overwrite=False)
 print(mydb.get_stats())
 
-print ("# Import and append data from local already existing pytreedb file")
+print("# Import and append data from local already existing pytreedb file")
 mydb.import_db(r'../data/db_dump/data.db', overwrite=False)
 print(mydb.get_stats())
 
 print("# Import and append pytreedb file from URL")
-mydb.import_db('https://heibox.uni-heidelberg.de/f/2be9b438f7d94e12a091/?dl=1', overwrite=False)
+mydb.import_db('https://github.com/3dgeo-heidelberg/pytreedb/raw/main/data/test/data.db', overwrite=False)
 print(mydb.get_stats())
 
 print("# Fresh import pytreedb file from URL")
-mydb.import_db('https://heibox.uni-heidelberg.de/f/2be9b438f7d94e12a091/?dl=1', overwrite=True)
+mydb.import_db('https://github.com/3dgeo-heidelberg/pytreedb/raw/main/data/test/data.db', overwrite=True)
 print(mydb.get_stats())
 
-print ("# Import and thus append other serialized pytreedb file")
+print("# Import and thus append other serialized pytreedb file")
 mydb.import_db(r'../data/db_dump/data_copy.db')
 print(mydb.get_stats())
 
-print ("# Import and append from local folder containing geojsons")
+print("# Import and append from local folder containing geojsons")
 mydb.import_data(r'..\data\geojson\trees\\', overwrite=False) 
 print(mydb.get_stats())
 
@@ -58,7 +59,8 @@ mydb.save("new_"+mydbfile)
 print("# Validate input GEOJSON if all mandatory keys are present")
 print(mydb.validate_json(r'..\data\geojson\AbiAlb_BR03_01.geojson'))
 
-print("# Show how to use a pytreedb util function: Generate SHA-1 hash for a file (e.g. could be used to check if content has changed")
+print("# Show how to use a pytreedb util function: Generate SHA-1 hash for a file "
+      "(e.g. could be used to check if content has changed")
 print(db.hash_file(mydb.dbfile))
 
 print("#DONE.")

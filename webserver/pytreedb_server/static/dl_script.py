b0164ec7d27e89b428d0b6ff91c4b9b49d9f2e43
Jiani Qu <jiani.qu@uni-heidelberg.de>
2022-04-22 13:52:23 +0200

    Script ready

--- webserver/pytreedb_server/static/dl_script.py
+++ webserver/pytreedb_server/static/dl_script.py
@@ -1,7 +1,28 @@
 import os
+import urllib.request
+import shutil
 
-source_file = None
-output_folder = None
+source_file = None  # location of the .txt file containing point cloud urls
+output_folder = None  # local folder to save the files under
+
+def download(urls: list[str], dest_folder: str):
+    """
+    Downloads point clouds (.laz) data from a list of source URLs
+
+    :param list[str] urls: list of URLs
+    :param str dest_folder: destination folder to save data
+    """
+    # create folder if it doesn't exist yet
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    for url in urls:
+        filename = url.split('/')[-1]
+        file_path = os.path.join(dest_folder, filename)
+        # download the file from url and save locally
+        with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+            print("saving to", os.path.abspath(file_path))
+
 
 if source_file is None:
     source_file = input("Enter the path of the point cloud URLs:")
@@ -11,7 +32,4 @@ if output_folder is None:
 
 f = open(source_file, "r")
 urls = f.read().split(",")
-
-def download(urls, dest_folder):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
+download(urls, output_folder)

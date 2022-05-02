5d1465dafe9adb8cb19c1233ff38f9982c8793d1
Jiani Qu <jiani.qu@uni-heidelberg.de>
2022-05-02 15:03:24 +0200

    Update download script #55

--- webserver/pytreedb_server/static/dl_script.py
+++ webserver/pytreedb_server/static/dl_script.py
@@ -1,9 +1,16 @@
-import os
-import urllib.request
+import sys
 import shutil
+import argparse
+import urllib.request
+from pathlib import Path, PurePath
+from urllib.parse import urlparse
 
-source_file = None  # location of the .txt file containing point cloud urls
-output_folder = None  # local folder to save the files under
+parser = argparse.ArgumentParser()
+parser.add_argument("-input", required=True, help="path of the input text file containing urls")
+parser.add_argument("-output", required=True, help="path to the download destination directory")
+args = parser.parse_args()
+source_file = args.input 
+output_folder = args.output
 
 def download(urls, dest_folder):
     """
@@ -12,24 +19,35 @@ def download(urls, dest_folder):
     :param list[str] urls: list of URLs
     :param str dest_folder: destination folder to save data
     """
-    # create folder if it doesn't exist yet
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
     for url in urls:
-        filename = url.split('/')[-1]
-        file_path = os.path.join(dest_folder, filename)
+        filename = PurePath(urlparse(url).path).name  # extract file name from the url
+        file_path = PurePath(dest_folder).joinpath(filename)
         # download the file from url and save locally
         with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
-            print("saving to", os.path.abspath(file_path))
+            print("saving to", Path(file_path).resolve())
 
 if __name__ == '__main__':
-    # Prompt user to enter paths if not given
-    if source_file is None:
-        source_file = input("Enter the path of the point cloud URLs:").strip('\"')
-    if output_folder is None:
-        output_folder = input("Enter the output directory:").strip('\"')
+    try:
+        f = open(source_file, "r")
+    except OSError:
+        print(f"Error: Could not open file: \"{source_file}\"")
+        print("-- Original error follows below --")
+        raise
+    
+    # create output folder if it doesn't exist yet
+    if not Path(output_folder).exists():
+        Path(output_folder).mkdir()
+        print(f"\"{output_folder}\" created")
 
-    with open(source_file, "r") as f:  # read input file
-        urls = f.read().split(",")     # save content into a list of urls
+    print(f"Input file path: \"{source_file}\" \nOutput directory path: \"{output_folder}\"")
+
+    with f:  # read input file
+        try:
+            urls = f.read().split(",")     # save content into a list of urls
+        except:
+            print(f"Error: Could not read file: \"{source_file}\"")
+            print("-- Original error follows below --")
+            raise
+
         download(urls, output_folder)  # download data to the output folder

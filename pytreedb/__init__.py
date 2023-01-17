064cc1a83e6335cd3f25535ef189343e90f6007a
han16nah <han_nah@mailbox.org>
2023-01-17 14:42:45 +0100

    adapt version

--- pytreedb/__init__.py
+++ pytreedb/__init__.py
@@ -20,19 +20,10 @@ MongoDB is used as database backend via the PyMongo driver. This enables scaling
 
 from __future__ import absolute_import
 
-__version__ = "0.1.a3"
+__version__ = "1.0.0"
 name = "pytreedb"
 
 import pytreedb.db
 import pytreedb.db_conf
 import pytreedb.db_utils
 
-__bibtex__ = r"""@Article{Hoefle:2022,
-  Author    = {H\"ofle, B},
-  Title     = {pytreedb},
-  Journal   = {xxx},
-  Volume    = {x},
-  Number    = {x},
-  Pages     = {xx--xx},
-  year      = 2022
-}"""
